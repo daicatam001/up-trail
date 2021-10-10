225b5c6aa90ae432a38c6f57b117034530c044fc
tampt <tampt95@gmail.com>
2021-10-10 14:28:24 +0700

    feat: improve view control

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -1,6 +1,13 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, HostListener, OnInit} from '@angular/core';
 import {Scene, SceneState, SceneStore} from "./scene.store";
-import {Observable} from "rxjs";
+import {fromEvent, Observable, of} from "rxjs";
+import {delay, exhaustMap, withLatestFrom} from "rxjs/operators";
+
+
+enum KEY_CODE {
+  ARROW_RIGHT = 39,
+  ARROW_LEFT = 37
+}
 
 @Component({
   selector: 'app-scene',
@@ -9,15 +16,46 @@ import {Observable} from "rxjs";
   providers: [SceneStore],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SceneComponent implements OnInit {
+export class SceneComponent implements AfterViewInit {
 
   vm$: Observable<SceneState & { scene: Scene }> = this.sceneStore.vm$
+  timer: any
+
 
   constructor(private sceneStore: SceneStore) {
   }
 
-  ngOnInit(): void {
+
+  ngAfterViewInit(): void {
+    fromEvent(document, 'keydown').pipe(exhaustMap(event => of(event).pipe(delay(300))),
+      withLatestFrom(this.vm$)
+    )
+      .subscribe(([event, vm]) => {
+        if ((event as KeyboardEvent).keyCode == KEY_CODE.ARROW_LEFT) {
+          this.viewMoreLeft()
+        }
+        if ((event as KeyboardEvent).keyCode == KEY_CODE.ARROW_RIGHT) {
+          this.viewMoreRight(vm.scene.sceneWidth!)
+        }
+      })
   }
+
+  public onUnHold() {
+    clearInterval(this.timer);
+  }
+
+  public onHoldViewMoreLeft() {
+    this.timer = setInterval(() => {
+      this.viewMoreLeft()
+    }, 300);
+  }
+
+  public onHoldViewMoreRight(sceneWidth: number) {
+    this.timer = setInterval(() => {
+      this.viewMoreRight(sceneWidth)
+    }, 300);
+  }
+
 
   viewMoreLeft() {
     this.sceneStore.viewMoreLeft()
